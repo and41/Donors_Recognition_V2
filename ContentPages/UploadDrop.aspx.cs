b2dc535f300c0c072ad476f567b90d9148b193a1
and41 <adufield@yahoo.com>
2017-11-20 15:57:58 -0500

    UploadCorrect

--- ContentPages/UploadDrop.aspx.cs
+++ ContentPages/UploadDrop.aspx.cs
@@ -19,6 +19,7 @@ public partial class ContentPages_UploadDrop : System.Web.UI.Page
 
     protected void UPLOADData_Click(object sender, EventArgs e)
     {
+        /*
         string excelPath = Server.MapPath("../Files/") + Path.GetFileName(FileUploadID.PostedFile.FileName);
         FileUploadID.SaveAs(excelPath);
 
@@ -28,142 +29,144 @@ public partial class ContentPages_UploadDrop : System.Web.UI.Page
         conString = string.Format(conString, excelPath);
         using (OleDbConnection excel_con = new OleDbConnection(conString))
         {
+        
             excel_con.Open();
             string sheet1 = excel_con.GetOleDbSchemaTable(OleDbSchemaGuid.Tables, null).Rows[0]["TABLE_NAME"].ToString();
             DataTable dtExcelData = new DataTable();
-            //Recognition Upload
-            if (RecognitionUpload.Visible == true)
+            */
+        //Recognition Upload
+        if (RecognitionUpload.Visible == true)
+        {
+            if (RecognitionUpload.Items.FindByValue("Scholarships").Selected == true)
             {
-                if (RecognitionUpload.Items.FindByValue("Scholarships").Selected == true)
-                {
-                    Call_Database conn = new Call_Database();
-                    conn.Connect(FileUploadID, "Scholarships");
+                Call_Database conn = new Call_Database();
+                conn.Connect(FileUploadID, "Scholarships");
             }
 
-                else if (RecognitionUpload.Items.FindByValue("Founders").Selected == true)
-                {
-                    Call_Database conn = new Call_Database();
-                    conn.Connect(FileUploadID, "Founders");
-                }
-                
-                else if (RecognitionUpload.Items.FindByValue("CornerstoneTab").Selected == true)
-                {
-                    Call_Database conn = new Call_Database();
-                    conn.Connect(FileUploadID, "CornerstoneTab");
-                }
-
-                else if (RecognitionUpload.Items.FindByValue("RoundtableTab").Selected == true)
-                {
-                    Call_Database conn = new Call_Database();
-                    conn.Connect(FileUploadID, "RoundtableTab");
-                }
-
-                else if (RecognitionUpload.Items.FindByValue("PresidentsTab").Selected == true)
-                {
-                    Call_Database conn = new Call_Database();
-                    conn.Connect(FileUploadID, "PresidentsTab");
-                }
-
-                else if (RecognitionUpload.Items.FindByValue("QuadrangleTab").Selected == true)
-                {
-                    Call_Database conn = new Call_Database();
-                    conn.Connect(FileUploadID, "QuadrangleTab");
-                }
-
-                else if (RecognitionUpload.Items.FindByValue("BlueGoldTab").Selected == true)
-                {
-                    Call_Database conn = new Call_Database();
-                    conn.Connect(FileUploadID, "BlueGoldTab");
-                }
-
-                else if (RecognitionUpload.Items.FindByValue("CenturyTab").Selected == true)
-                {
-                    Call_Database conn = new Call_Database();
-                    conn.Connect(FileUploadID, "CenturyTab");
-                }
-
-                else if (RecognitionUpload.Items.FindByValue("OtherDonorsTab").Selected == true)
-                {
-                    Call_Database conn = new Call_Database();
-                    conn.Connect(FileUploadID, "OtherDonorsTab");
-                }
-                //Recognition End
-            }
-            //Leadership Here
-            else if (LeadershipUpload.Visible == true)
+            else if (RecognitionUpload.Items.FindByValue("Founders").Selected == true)
             {
-                if (LeadershipUpload.Items.FindByValue("AdvBoard").Selected == true)
-                {
-                    Call_Database conn = new Call_Database();
-                    conn.Connect(FileUploadID, "AdvBoard");
-                }
-
-                else if (LeadershipUpload.Items.FindByValue("ElecBoard").Selected == true)
-                {
-                    Call_Database conn = new Call_Database();
-                    conn.Connect(FileUploadID, "ElecBoard");
-                }
+                Call_Database conn = new Call_Database();
+                conn.Connect(FileUploadID, "Founders");
             }
-            //Leadership END
-            //MAP START
-            else if (MapDataUpload.Visible == true)
+
+            else if (RecognitionUpload.Items.FindByValue("CornerstoneTab").Selected == true)
             {
-                if (MapDataUpload.Items.FindByValue("OutdoorsInfo").Selected == true)
-                {
-                    Call_Database conn = new Call_Database();
-                    conn.Connect(FileUploadID, "OutdoorsInfo");
-                }
-
-                else if (MapDataUpload.Items.FindByValue("ChapelInfo").Selected == true)
-                {
-                    Call_Database conn = new Call_Database();
-                    conn.Connect(FileUploadID, "ChapelInfo");
-                }
-
-                else if (MapDataUpload.Items.FindByValue("CommonsInfo").Selected == true)
-                {
-                    Call_Database conn = new Call_Database();
-                    conn.Connect(FileUploadID, "CommonsInfo");
-                }
-
-                else if (MapDataUpload.Items.FindByValue("FisherHallInfo").Selected == true)
-                {
-                    Call_Database conn = new Call_Database();
-                    conn.Connect(FileUploadID, "FisherHallInfo");
-                }
-
-                else if (MapDataUpload.Items.FindByValue("FitnessCenterInfo").Selected == true)
-                {
-                    Call_Database conn = new Call_Database();
-                    conn.Connect(FileUploadID, "FitnessCenterInfo");
-                }
-
-                else if (MapDataUpload.Items.FindByValue("LibraryInfo").Selected == true)
-                {
-                    Call_Database conn = new Call_Database();
-                    conn.Connect(FileUploadID, "LibraryInfo");
-                }
-
-                else if (MapDataUpload.Items.FindByValue("ResHallInfo").Selected == true)
-                {
-                    Call_Database conn = new Call_Database();
-                    conn.Connect(FileUploadID, "ResHallInfo");
-                }
-
-                else if (MapDataUpload.Items.FindByValue("SwartsHallInfo").Selected == true)
-                {
-                    Call_Database conn = new Call_Database();
-                    conn.Connect(FileUploadID, "SwartsHallInfo");
-                }
-
-                else if (MapDataUpload.Items.FindByValue("BlaisdellHallInfo").Selected == true)
-                {
-                    Call_Database conn = new Call_Database();
-                    conn.Connect(FileUploadID, "BlaisdellHallInfo");
-                }
+                Call_Database conn = new Call_Database();
+                conn.Connect(FileUploadID, "CornerstoneTab");
             }
-        //MAP END
+
+            else if (RecognitionUpload.Items.FindByValue("RoundtableTab").Selected == true)
+            {
+                Call_Database conn = new Call_Database();
+                conn.Connect(FileUploadID, "RoundtableTab");
+            }
+
+            else if (RecognitionUpload.Items.FindByValue("PresidentsTab").Selected == true)
+            {
+                Call_Database conn = new Call_Database();
+                conn.Connect(FileUploadID, "PresidentsTab");
+            }
+
+            else if (RecognitionUpload.Items.FindByValue("QuadrangleTab").Selected == true)
+            {
+                Call_Database conn = new Call_Database();
+                conn.Connect(FileUploadID, "QuadrangleTab");
+            }
+
+            else if (RecognitionUpload.Items.FindByValue("BlueGoldTab").Selected == true)
+            {
+                Call_Database conn = new Call_Database();
+                conn.Connect(FileUploadID, "BlueGoldTab");
+            }
+
+            else if (RecognitionUpload.Items.FindByValue("CenturyTab").Selected == true)
+            {
+                Call_Database conn = new Call_Database();
+                conn.Connect(FileUploadID, "CenturyTab");
+            }
+
+            else if (RecognitionUpload.Items.FindByValue("OtherDonorsTab").Selected == true)
+            {
+                Call_Database conn = new Call_Database();
+                conn.Connect(FileUploadID, "OtherDonorsTab");
+            }
+            //Recognition End
         }
+        //Leadership Here
+        else if (LeadershipUpload.Visible == true)
+        {
+            if (LeadershipUpload.Items.FindByValue("AdvBoard").Selected == true)
+            {
+                Call_Database conn = new Call_Database();
+                conn.Connect(FileUploadID, "AdvBoard");
+            }
+
+            else if (LeadershipUpload.Items.FindByValue("ElecBoard").Selected == true)
+            {
+                Call_Database conn = new Call_Database();
+                conn.Connect(FileUploadID, "ElecBoard");
+            }
+        }
+        //Leadership END
+        //MAP START
+        else if (MapDataUpload.Visible == true)
+        {
+            if (MapDataUpload.Items.FindByValue("OutdoorsInfo").Selected == true)
+            {
+                Call_Database conn = new Call_Database();
+                conn.Connect(FileUploadID, "OutdoorsInfo");
+            }
+
+            else if (MapDataUpload.Items.FindByValue("ChapelInfo").Selected == true)
+            {
+                Call_Database conn = new Call_Database();
+                conn.Connect(FileUploadID, "ChapelInfo");
+            }
+
+            else if (MapDataUpload.Items.FindByValue("CommonsInfo").Selected == true)
+            {
+                Call_Database conn = new Call_Database();
+                conn.Connect(FileUploadID, "CommonsInfo");
+            }
+
+            else if (MapDataUpload.Items.FindByValue("FisherHallInfo").Selected == true)
+            {
+                Call_Database conn = new Call_Database();
+                conn.Connect(FileUploadID, "FisherHallInfo");
+            }
+
+            else if (MapDataUpload.Items.FindByValue("FitnessCenterInfo").Selected == true)
+            {
+                Call_Database conn = new Call_Database();
+                conn.Connect(FileUploadID, "FitnessCenterInfo");
+            }
+
+            else if (MapDataUpload.Items.FindByValue("LibraryInfo").Selected == true)
+            {
+                Call_Database conn = new Call_Database();
+                conn.Connect(FileUploadID, "LibraryInfo");
+            }
+
+            else if (MapDataUpload.Items.FindByValue("ResHallInfo").Selected == true)
+            {
+                Call_Database conn = new Call_Database();
+                conn.Connect(FileUploadID, "ResHallInfo");
+            }
+
+            else if (MapDataUpload.Items.FindByValue("SwartsHallInfo").Selected == true)
+            {
+                Call_Database conn = new Call_Database();
+                conn.Connect(FileUploadID, "SwartsHallInfo");
+            }
+
+            else if (MapDataUpload.Items.FindByValue("BlaisdellHallInfo").Selected == true)
+            {
+                Call_Database conn = new Call_Database();
+                conn.Connect(FileUploadID, "BlaisdellHallInfo");
+            }
+        }
+        //MAP END
+    //}
     }
 
 
